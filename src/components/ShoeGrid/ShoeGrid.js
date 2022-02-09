7d6ea8e38e548dddea6d7cc5cea3eee22c655503
Gökhan Ünal <unal_gokhan@outlook.com>
2022-02-10 00:41:39 +0300

    shoegrid

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -8,12 +8,25 @@ const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeWrapper>
+          <ShoeCard key={shoe.slug} {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 36px;
+  /* margin: -16px; */
+`;
+
+const ShoeWrapper = styled.div`
+  min-width: 275px;
+  /* margin: 16px;  for older browsers instead of gap but we should give margin -16 to parent also */
+  flex: 1;
+`;
 
 export default ShoeGrid;
